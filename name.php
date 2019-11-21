69bfbc870d51c17cd90eac4727f6afe93e38d7dd
Iz-Dine Amadou <2088346@talnet.nl>
2019-11-21 12:08:10 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+echo "wat is je naam " . PHP_EOL;
+$voornaam = readline(">");
+echo "wat is je achter naam " . PHP_EOL;
+$achternaam = readline('>');
+echo "jou naam is dus .$voornaam .$achternaam ";
